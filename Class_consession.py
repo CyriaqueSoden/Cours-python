34dc90a399667c12d0f0dcd97c986dad3c0166d5
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2020-11-09 15:33:52 +0100

    version qui marche mais dans calsse

--- Class_consession.py
+++ Class_consession.py
@@ -5,9 +5,19 @@ class voiture:
     def __init__(self, marque, modele, couleur):
         self.marque = marque
         self.modele = modele
-        #self.prix_modele = prix_modele
         self.couleur = couleur
-        #self.prix_couleur = prix_couleur
-        #self.prix_voitur = self.prix_voiture()
+        self.prix_voiture = self.prix_voiture()
+
+    def prix_voiture(self):
+        return marque[a][1]*modele[b]*couleur[c][1]
 
 
+marque = [("Peugeot", 1), ("Renault", 1), ("Opel", 1), ("Citroën", 1.2),
+          ("Volkswagen", 1.5), ("Mercedes", 2), ("BMW", 3), ("Fiat", 0.8)]
+modele = [100, 200, 300, 400, 500]
+couleur = [("noir", 1), ("rouge", 1), ("blanc", 1), ("gris", 1.2),
+           ("carbon", 1.5), ("orange", 2), ("jaune", 3), ("vert", 0.8)]
+
+a = random.randint(0, len(marque)-1)
+b = random.randint(0, len(modele)-1)
+c = random.randint(0, len(couleur)-1)
--- Concessionnaire.py
+++ Concessionnaire.py
@@ -2,22 +2,8 @@ from Class_consession import *
 import random
 
 
-def prix_voiture(self, v):
-    return marque[a][1]*modele[b]*couleur[c][1]
+v = voiture(marque[a][0],modele[b],couleur[c][0])
 
 
-marque = [("Peugeot", 1), ("Renault", 1), ("Opel", 1), ("Citroën", 1.2),
-          ("Volkswagen", 1.5), ("Mercedes", 2), ("BMW", 3), ("Fiat", 0.8)]
-
-modele = [100, 200, 300, 400, 500]
-couleur = [("noir", 1), ("rouge", 1), ("blanc", 1), ("gris", 1.2),
-           ("carbon", 1.5), ("orange", 2), ("jaune", 3), ("vert", 0.8)]
-
-a = random.randint(0, len(marque)-1)
-b = random.randint(0, len(modele)-1)
-c = random.randint(0, len(couleur)-1)
-
-v = voiture(marque[a][0], modele[b], couleur[c][0], )
 
 print(v.__dict__)
-print(prix_voiture(v))
