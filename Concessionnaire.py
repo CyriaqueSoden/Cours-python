5024b4a095ea61f63bb270534106e2eae5423225
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2020-11-09 14:29:03 +0100

    .

--- Concessionnaire.py
+++ Concessionnaire.py
@@ -1,18 +1,9 @@
-class voiture:
-    def __init__(self, marque, modele, couleur, prix_couleur, prix_modele):
-        self.marque = marque
-        self.modele = modele
-        self.prix_modele = prix_modele
-        self.couleur = couleur
-        self.prix_couleur = prix_couleur
-        self.prix_voitur = self.prix_voiture()
+from Class_consession import *
+import random
 
-    def prix_voiture(self):
-        return self.prix_modele*self.prix_couleur
+# # marque, modele, couleur, prix_couleur, prix_modele
 
 
-if __name__ == "__main__":
-    v = voiture("BMW", "Panda", "rouge", 2.5, 2000)
 
 
-print(v.__dict__)
+
