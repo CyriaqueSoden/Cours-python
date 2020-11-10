1ecf08815f577e412e15242e137b2fa0735cfdf7
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2020-11-10 11:26:04 +0100

    nom variable

--- Recherche_de_voiture.py
+++ Recherche_de_voiture.py
@@ -3,15 +3,15 @@ from Class_client import client
 
 liste_voiture =[]
 
-v = voiture()
-h = client("pithon", "mathieu")
-w = 0
+vroum = voiture()
+mathieu = client("pithon", "mathieu")
+compteur_de_boucle = 0
 
-while h.budget < v.prix_voiture :
-    w += 1
-    v = voiture()
-    liste_voiture.append(v)
-    if w == 5 :
+while mathieu.budget < vroum.prix_voiture :
+    compteur_de_boucle += 1
+    vroum = voiture()
+    liste_voiture.append(vroum)
+    if compteur_de_boucle == 5 :
         print("pas de voiture dans le budget")
         break
     
@@ -19,5 +19,5 @@ while h.budget < v.prix_voiture :
 
 
 
-print(v.__dict__)
-print(h.__dict__)
+print(vroum.__dict__)
+print(mathieu.__dict__)
